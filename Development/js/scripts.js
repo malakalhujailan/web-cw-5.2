add5567e39b44db80b50b3588e5dd07bccfbbbe6
malakalhujailan <66518892+malakalhujailan@users.noreply.github.com>
2020-06-29 22:12:01 +0300

    تم و أخيراً

---
+++ Development/js/scripts.js
@@ -0,0 +1,64 @@
+let age= parseInt(prompt("please enter your age"));
+if(age<=7){
+     console.log("روضة")
+}
+else if (age<=12){
+     console.log("ابتدائي")
+}
+else if(age<=16){
+     console.log("متوسطة")
+}
+else if(age<=18){
+     console.log("ثانوية")
+}
+else if(age<=30){
+     console.log("جامعة")
+}
+
+
+           let food =(prompt( "Please enter a number to order?"));
+           if (food=1){
+           console.log("Thank you for ordering a pizza)" )
+                                    }
+            if (food= 2){
+             console.log ( "Thank you for ordering pasta")
+                                    }
+             if (food= 3){
+            console.log ( "Thank you for ordering pasta")
+                                    } 
+            
+           
+            
+            
+            
+            let n1 = parselnt(prompt( "Enter the first number"));
+            let n2 = parselnt(prompt( 'Enter the secound number'));
+            let operation=parselnt(prompt( 'choose an operation * or - or or = or /'));
+            if (operation = '-') {
+            console.log (n1-n2)
+            }
+            else if (operation = '*') {
+                console.log (n1*n2)}
+              
+            else if (operation = '+') {
+                    console.log (n1+n2)}
+                   
+             else if (operation = '/') {
+                        console.log (n1/n2)}
+
+            let age= parseInt(prompt("please enter your age"));
+            if(age=>7){
+                 console.log("روضة")
+            }
+         else if (age=>12){
+                 console.log("ابتدائي")
+            }
+           else if(age=>16){
+                 console.log("متوسطة")
+            }
+           else if(age=>18){
+                 console.log("ثانوية")
+            }
+            else if(age=>30){
+                 console.log("جامعة")
+            }
